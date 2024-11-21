52106a1de06fe1da9a21dae9789dd9b6e824c5bc
prakashgupta27 <prakashgupta272001@gmail.com>
2024-11-21 17:41:52 +0530

    Console error solve apply alt tags for image

--- src/components/Home1/Benefit.tsx
+++ src/components/Home1/Benefit.tsx
@@ -24,23 +24,24 @@ const Benefit: React.FC<Props> = ({ props }) => {
                 <div className={`benefit-block ${props}`}>
                     <div className="list-benefit grid items-start lg:grid-cols-4 grid-cols-2 gap-[30px]">
                         <div className="benefit-item flex flex-col items-center justify-center">
-                            <Image src={indiatIcon} width={80} />
+                        <Image src={indiatIcon} width={80} alt='image of india-map-icon png' className="hover:scale-110 transition-transform duration-200"/>
+
                             <div className="heading6 text-center mt-5">India Tour</div>
                             <div className="caption1 text-secondary text-center mt-3">"Explore India&#39;s rich culture, history, and scenic beauty with us."</div>
                         </div>
                         <div className="benefit-item flex flex-col items-center justify-center">
-                            <Image src={international} width={80} />
+                            <Image src={international} width={80} alt='international-tour-icon png' className="hover:scale-110 transition-transform duration-200"/>
                             <div className="heading6 text-center mt-5">International Tour</div>
                             <div className="caption1 text-secondary text-center mt-3">Explore diverse cultures, iconic landmarks, and unforgettable experiences on international tours.</div>
                         </div>
                         <div className="benefit-item flex flex-col items-center justify-center">
-                            <Image src={yatra} width={80} />
+                            <Image src={yatra} width={80} alt='yatra-tour-icon png' className="hover:scale-110 transition-transform duration-200"/>
 
                             <div className="heading6 text-center mt-5">Yatra</div>
                             <div className="caption1 text-secondary text-center mt-3"> Embark on a memorable Yatra, exploring spiritual destinations and enriching experiences.</div>
                         </div>
                         <div className="benefit-item flex flex-col items-center justify-center">
-                            <Image src={honeymoon} width={80} />
+                            <Image src={honeymoon} width={80} alt='honnymoon-tour-icon png' className="hover:scale-110 transition-transform duration-200"/>
                             <div className="heading6 text-center mt-5">Honeymoon Package</div>
                             <div className="caption1 text-secondary text-center mt-3">Experience romance and luxury with our unforgettable honeymoon packages worldwide.</div>
                         </div>
